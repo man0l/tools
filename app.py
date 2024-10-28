e2716b475885f51bb73981cb353ac74a378bae61
Manol Trendafilov <manol@camplight.dev>
2024-10-28 19:59:57 +0200

    change the listeing for any host

--- app.py
+++ app.py
@@ -208,4 +208,5 @@ def health_check():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get('FLASK_RUN_PORT', 5001))
+    app.run(host='0.0.0.0', port=port)
